58bb81f949fdc9e3f6c99a196500d911748fb572
qingxu_L <836192293@qq.com>
2022-04-11 15:47:35 +0800

    feat: nacos作为注册中心

---
+++ nacos_ribbon/nacos_ribbon_hi_service_a/src/main/java/com/xub/springcloud/study/nacos_ribbon/nacos_ribbon_hi_service_a/NacosRibbonHiServiceAApplication.java
@@ -0,0 +1,14 @@
+package com.xub.springcloud.study.nacos_ribbon.nacos_ribbon_hi_service_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosRibbonHiServiceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosRibbonHiServiceAApplication.class, args);
+    }
+}
