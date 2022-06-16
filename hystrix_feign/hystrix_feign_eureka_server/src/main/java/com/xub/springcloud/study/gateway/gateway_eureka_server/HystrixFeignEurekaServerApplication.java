dbed0f2b3d3931e7ea705dbcc010091ae89621b0
qingxu_L <836192293@qq.com>
2022-06-16 22:33:13 +0800

    fix：配置问题

--- hystrix_feign/hystrix_feign_eureka_server/src/main/java/com/xub/springcloud/study/gateway/gateway_eureka_server/HystrixFeignEurekaServerApplication.java
+++ hystrix_feign/hystrix_feign_eureka_server/src/main/java/com/xub/springcloud/study/gateway/gateway_eureka_server/HystrixFeignEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.xub.springcloud.study.hystrix_feign.hystrix_feign_eureka_server;
+package com.xub.springcloud.study.gateway.gateway_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
